d1157d73e9fcf2c0c01d258f7d5dbc8b2ff0db4c
SANJU PAPPACHAN <sanjupappachan8943@gmail.com>
2019-10-20 13:09:16 +0530

    Add files via upload

--- modules/banner.py
+++ modules/banner.py
@@ -1,10 +1,10 @@
-# WebKiller
+# -*- coding: utf-8 -*- 
 
-Tool Information Gathering Write With Python.
+from colors import *
 
+banner = r + """
+  
 
-## PreView
-<pre>
 ██╗    ██╗███████╗██████╗ ██╗  ██╗██╗██╗     ██╗     ███████╗██████╗ 
 ██║    ██║██╔════╝██╔══██╗██║ ██╔╝██║██║     ██║     ██╔════╝██╔══██╗
 ██║ █╗ ██║█████╗  ██████╔╝█████╔╝ ██║██║     ██║     █████╗  ██████╔╝
@@ -18,56 +18,4 @@ Tool Information Gathering Write With Python.
 **               Team Members : Abolfaz Hajizadeh , MrQadir       **
 **                                                                **
 ====================================================================
-
-    1 - Reverse IP With HackTarget
-    2 - Reverse IP With YouGetSignal
-    3 - Geo IP Lookup
-    4 - Whois
-    5 - Bypass CloudFlare
-    6 - DNS Lookup
-    7 - Find Shared DNS
-    8 - Show HTTP Header
-    9 - Port Scan
-    10 - CMS Scan
-    11 - Page Admin Finder
-    12 - Robots.txt
-    13 - Traceroute
-    14 - Honeypot Detector
-    15 - Ping
-    16 - All
-    17 - Exit
-    
-Enter : 
-</pre>
-
-
-## Operating Systems Tested
-- Kali Linux 2018.2
-- Windows 10
-
-
-## Install
-```bash
-git clone https://github.com/ultrasecurity/webkiller.git
-cd webkiller
-pip install -r requirements.txt
-python webkiller.py 
-```
-
-## ScreenShot
-![webkiller](https://user-images.githubusercontent.com/34939571/42339890-a8da008c-80a3-11e8-87c1-dcf5e0648203.png)
-
-## Video Tutorial
-Youtube : https://www.youtube.com/watch?v=MTaM1YX8QsE
-
-
-### Thanks to
-    Ashkan Moghaddas - Ultra Security Team Leader
-    Abolfazl Hajizadeh - Ultra Security Team Programmer
-    Milad Ranjbar -  Ultra Security Team Programmer
-    MrQadir - Team Member 
-
-### Contact us
-- WebSite Ultra Security Team : https://ultrasec.org
-- Channel Telegram : https://t.me/UltraSecurity
-- Contact with Milad Ranjbar : https://t.me/milad_zero
+""" + g
